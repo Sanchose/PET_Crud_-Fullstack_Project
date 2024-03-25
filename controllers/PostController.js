4792430993fae4356943f866af66c0d3249a4744
Sanchose <sashapronehoda@gmail.com>
2024-03-25 11:22:13 +0100

    created Post.js and modified PostController.js, index.js

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,7 +1,64 @@
-export const create = (req, res) => {
-    try{
+import PostModel from "../models/Post.js";
 
-    } catch (err) {
-        
-    }
-}
+export const getAll = async (req, res) => {
+  try {
+    const posts = await PostModel.find().populate("user").exec();
+    res.json(posts);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "No posts founded",
+    });
+  }
+};
+
+export const getOne = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    PostModel.findOneAndUpdate(
+      {
+        _id: postId,
+      },
+      {
+        $inc: { viewsCount: 1 },
+      },
+      {
+        returnDocument: "after",
+      },
+      (err, doc) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({
+            message: "Failed to return a post",
+          });
+        }
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "No posts founded",
+    });
+  }
+};
+
+export const create = async (req, res) => {
+  try {
+    const doc = new PostModel({
+      title: req.body.title,
+      text: req.body.text,
+      imageUrl: req.body.imageUrl,
+      tags: req.body.tags,
+      user: req.userId,
+    });
+
+    const post = await doc.save();
+    res.json(post);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Error, no new post",
+    });
+  }
+};
